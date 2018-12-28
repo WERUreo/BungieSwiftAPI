985323343c05985a5f2acc1f75a29f714e9c3c16
Keli'i Martin <kelii.d.martin@gmail.com>
2018-12-28 10:12:52 -0500

    Initial Commit

---
+++ BungieSwiftAPI/BungieSwiftAPI.h
@@ -0,0 +1,19 @@
+//
+//  BungieSwiftAPI.h
+//  BungieSwiftAPI
+//
+//  Created by Kelii Martin on 12/28/18.
+//  Copyright © 2018 WERUreo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BungieSwiftAPI.
+FOUNDATION_EXPORT double BungieSwiftAPIVersionNumber;
+
+//! Project version string for BungieSwiftAPI.
+FOUNDATION_EXPORT const unsigned char BungieSwiftAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BungieSwiftAPI/PublicHeader.h>
+
+
